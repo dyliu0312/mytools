3ba96b0ace09add1b3c689e7370f33f8cab84e16
dyliu0312 <liudy0312@gmail.com>
2025-12-02 01:54:29 +0800

    [feat]: add signal estimation based on a fixed width, get rid of gaussian fitting of the y profile

---
+++ src/mytools/estimate_fixwidth.py
@@ -0,0 +1,240 @@
+"""
+estimate the signal level baed on the given width.
+"""
+
+from typing import Optional, Sequence
+
+import numpy as np
+
+
+def get_int(x):
+    """
+    Round a float or array of floats to the nearest integer.
+
+    Parameters
+    ----------
+    x : Union[float, np.ndarray]
+        The input number or array.
+
+    Returns
+    -------
+    Union[int, np.ndarray]
+        The rounded integer or array of integers.
+    """
+    x = np.array(x)
+    return np.rint(x).astype(int)
+
+
+def get_start_end_pixel_index(
+    x_min: float = -0.5,
+    x_max: float = 0.5,
+    shape: int = 120,
+    lim: Sequence[float] = (-3, 3),
+    offset: int = 1,
+):
+    """
+    Get the start and end pixel indices for a given coordinate range.
+
+    Parameters
+    ----------
+    x_min : float, optional
+        The minimum world coordinate. Defaults to -0.5.
+    x_max : float, optional
+        The maximum world coordinate. Defaults to 0.5.
+    shape : int, optional
+        The number of pixels along the axis. Defaults to 120.
+    lim : Sequence[float], optional
+        The world coordinate limits of the axis, as (min, max).
+        Defaults to (-3, 3).
+    offset : int, optional
+        An offset to add to the calculated end index. Defaults to 1.
+
+    Returns
+    -------
+    Tuple[int, int]
+        The calculated start and end pixel indices.
+    """
+    xx = np.linspace(lim[0], lim[1], shape)
+
+    x_start = np.argmin(np.abs(xx - x_min))
+    x_end = np.argmin(np.abs(xx - x_max)) + offset
+
+    return x_start, x_end
+
+
+def get_center_outer_background(
+    data, data_err, width, x_range=[-0.5, 0.5], xlim=[-3, 3], ylim=[-3, 3], shift_unit=2
+):
+    """
+    Extract center, outer, and background regions from the data.
+
+    The 'center' is defined by the `x_range` and a given `width` in y.
+    The 'outer' regions are shifted versions of the center region along the x-axis.
+    The 'background' is taken from regions in y far from the center.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        The 2D data array.
+    data_err : np.ndarray
+        The 1-sigma error of the 2D data array.
+    width : float
+        The half-width (e.g., 1-sigma) of the central region in world coordinates.
+    x_range : list, optional
+        The x-range of the central region. Defaults to [-0.5, 0.5].
+    xlim : list, optional
+        The x-limits of the data array. Defaults to [-3, 3].
+    ylim : list, optional
+        The y-limits of the data array. Defaults to [-3, 3].
+    shift_unit : int, optional
+        The shift distance for the outer regions, in world coordinates.
+        Defaults to 2.
+
+    Returns
+    -------
+    Tuple[Tuple, Tuple]
+        A tuple containing the extracted regions and their errors:
+        ((center, left, right, background), (center_err, left_err, right_err, background_err)).
+    """
+    sy, sx = data.shape
+    npix_unit_x = sx / (xlim[1] - xlim[0])
+    npix_unit_y = sy / (ylim[1] - ylim[0])
+    width_pix = get_int(width * npix_unit_y)
+    # print("width: %s" % width)
+    # print("width_pix: %s" % width_pix)
+
+    # cut the data to the given x range to estimate the width
+    x_st, x_ed = get_start_end_pixel_index(*x_range, shape=sx, lim=xlim, offset=1)
+    data_cut = data[:, x_st:x_ed]
+    data_err_cut = data_err[:, x_st:x_ed]
+
+    # get the center, outer and background region
+    # The center area uses the Gaussian 1-sigma width
+    y_st = sy // 2 - width_pix
+    y_ed = sy // 2 + width_pix
+    center = data_cut[y_st:y_ed, :]
+    center_err = data_err_cut[y_st:y_ed, :]
+
+    # The background area uses the 3-4 sigma range
+    bk_top_st = y_ed + width_pix * 3
+    bk_top_ed = y_ed + width_pix * 4
+    bk_bottom_st = y_st - width_pix * 4
+    bk_bottom_ed = y_st - width_pix * 3
+    bk_list = list(range(bk_top_st, bk_top_ed)) + list(
+        range(bk_bottom_st, bk_bottom_ed)
+    )
+    background = data[bk_list, x_st:x_ed]
+    background_err = data_err[bk_list, x_st:x_ed]
+
+    # The outer areas are the center region shifted along the x-axis
+    left_ids = get_int(
+        [x_st - shift_unit * npix_unit_x, x_ed - shift_unit * npix_unit_x]
+    )
+    right_ids = get_int(
+        [x_st + shift_unit * npix_unit_x, x_ed + shift_unit * npix_unit_x]
+    )
+    left = data[y_st:y_ed, left_ids[0] : left_ids[1]]
+    left_err = data_err[y_st:y_ed, left_ids[0] : left_ids[1]]
+    right = data[y_st:y_ed, right_ids[0] : right_ids[1]]
+    right_err = data_err[y_st:y_ed, right_ids[0] : right_ids[1]]
+
+    clrb = (center, left, right, background)
+    clrb_err = (center_err, left_err, right_err, background_err)
+
+    return clrb, clrb_err
+
+
+def get_signal_level_with_width(
+    data: np.ndarray,
+    width: float,
+    data_err: Optional[np.ndarray] = None,
+    x_range: Sequence[float] = [-0.5, 0.5],
+    x_num: int = 20,
+    xlim: Sequence[float] = [-3, 3],
+    ylim: Sequence[float] = [-3, 3],
+    shift_unit: int = 2,
+    show_profile: bool = True,
+):
+    """
+    Get the signal level with a given width.
+
+    This function uses a given width to define and extract center, outer,
+    and background regions to determine signal levels. It does not perform
+    any fitting.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        The 2D data array.
+    width : float
+        The half-width (e.g., 1-sigma) of the central region in world coordinates.
+    data_err : Optional[np.ndarray], optional
+        1-sigma error of the data. Defaults to None.
+    x_range : Sequence[float], optional
+        The x-range for profile averaging and analysis. Defaults to [-0.5, 0.5].
+    x_num : int, optional
+        Number of points for the output x-axis. Defaults to 20.
+    xlim : Sequence[float], optional
+        The x-limits of the data array. Defaults to [-3, 3].
+    ylim : Sequence[float], optional
+        The y-limits of the data array. Defaults to [-3, 3].
+    shift_unit : int, optional
+        The shift distance for the outer regions. Defaults to 2.
+    show_profile : bool, optional
+        If True, plot the y-profile. Defaults to True.
+
+    Returns
+    -------
+    Tuple
+        - A list of x-arrays for plotting profiles.
+        - A list of y-arrays for plotting profiles.
+        - A list of y-error arrays for plotting profiles.
+        - A list of flattened data from the center, left, right, and background regions.
+        - A list of flattened error data from the CLRB regions.
+    """
+    if data_err is None:
+        data_err = np.zeros_like(data)
+
+    sy, sx = data.shape
+
+    # Calculate y-profile for inspection
+    x_st, x_ed = get_start_end_pixel_index(*x_range, shape=sx, lim=xlim, offset=1)
+    data_cut = data[:, x_st:x_ed]
+    y_profile = data_cut.mean(axis=1)
+    yy = np.linspace(*ylim, num=sy)
+
+    y_profile_err = None
+    if data_err is not None:
+        if data_err.shape != data.shape:
+            raise ValueError(
+                f"Shape of data_err {data_err.shape} must match shape of data {data.shape}."
+            )
+        data_err_cut = data_err[:, x_st:x_ed]
+        y_profile_err = np.sqrt(np.sum(data_err_cut**2, axis=1)) / data_err_cut.shape[1]
+
+    # get the center, left, right and background
+    clrb, clrb_err = get_center_outer_background(
+        data, data_err, width, x_range, xlim, ylim, shift_unit
+    )
+
+    # get the filament signal level and background level
+    tf = np.mean(clrb[0], axis=0)
+    tbg = np.mean(clrb[-1], axis=0)
+
+    # propagate errors for tf and tbg
+    tf_err = np.sqrt(np.sum(clrb_err[0] ** 2, axis=0)) / clrb_err[0].shape[0]
+    tbg_err = np.sqrt(np.sum(clrb_err[-1] ** 2, axis=0)) / clrb_err[-1].shape[0]
+
+    # get the x axis
+    xx = np.linspace(*x_range, num=x_num)
+
+    # create the line data
+    line_x = [yy, xx, xx]
+    line_y = [y_profile, tf, tbg]
+    line_y_err = [y_profile_err, tf_err, tbg_err]
+
+    # get flattened data
+    clrb_flat = [x.flatten() for x in clrb]
+    clrb_err_flat = [x.flatten() for x in clrb_err]
+
+    return line_x, line_y, line_y_err, clrb_flat, clrb_err_flat
--- src/mytools/plot_custom.py
+++ src/mytools/plot_custom.py
@@ -34,7 +34,7 @@ def plot_stack_fit_res(
     vmax: Union[float, List[float], None] = None,
     q: float = 5,
     show_cbar: bool = True,
-    cbar_label: Union[str, List[str]] = r"$T\,[\mu$K]",
+    cbar_label: Union[str, List[str]] = r"$T\ [\mu$K]",
     kw_makefigure: Optional[dict] = dict(sharey=True),  # pyright: ignore[reportMissingTypeArgument]
     title: Union[str, List[str]] = [
         "Pairwise-stacked map",
@@ -80,7 +80,7 @@ def plot_res(
     show_cbar: Union[bool, List[bool]] = [False, True],
     cmap: Union[str, List[str]] = "viridis",
     cbar_loc: str = "right",
-    cbar_label: Union[str, List[str]] = r"$T\,[\mu$K]",
+    cbar_label: Union[str, List[str]] = r"$T\ [\mu$K]",
     title: Union[str, List[str]] = ["HI only", "HI + noise"],
     xlabel: Union[str, List[str]] = "X",
     ylabel: Union[str, List[str]] = "Y",
@@ -134,7 +134,7 @@ def plot_profile_2c(
     width: float = 0.32,
     fontsize: Union[float, List[float]] = 12,
     xlabel: Union[str, List[Union[str, None]], None] = ["Y", "X"],
-    ylabel: Union[str, List[Union[str, None]], None] = [r"$T\,[\mu$K]", None],
+    ylabel: Union[str, List[Union[str, None]], None] = [r"$T\ [\mu$K]", None],
     title: Union[str, List[Union[str, None]], None] = [
         "Transverse-section profile",
         "Lengthwise-section profile",
@@ -166,7 +166,7 @@ def plot_profile_2c(
         width (float): width of the filament to show on the left panel. Defaults to 0.32.
         fontsize (str): fontsize of the text. Defaults to 12.
         xlabel (str, list): x-axis labels. Defaults to ["Y", "X"].
-        ylabel (str, list): y-axis labels. Defaults to [r"$T$[$\\mu$K]", None].
+        ylabel (str, list): y-axis labels. Defaults to [r"$T$ [$\\mu$K]", None].
         title (str, list): subplot titles. Defaults to ["Transverse-section profile", \
             "Lengthwise-section profile"].
         color (str, list): line colors. Defaults to ["b", "r", "r", "k"].
@@ -316,7 +316,7 @@ def plot_profile_2r(
     fontsize: Union[float, List[float]] = 12,
     title: Union[str, List[str], None] = None,
     xlabel: Union[str, List[Union[str, None]], None] = ["Y", "X"],
-    ylabel: Union[str, List[Union[str, None]], None] = r"$T\,[\mu$K]",
+    ylabel: Union[str, List[Union[str, None]], None] = r"$T\ [\mu$K]",
     color: Union[str, List[str], None] = None,
     marker: Union[str, List[str], None] = None,
     linestyle: Union[str, List[str]] = "--",
@@ -340,7 +340,7 @@ def plot_profile_2r(
         title (str, list): titles of the subplots. Defaults to None.
         xlabel_up (str, list): x labels of the upper pannels. Defaults to "Y".
         xlabel_down (str, list): x labels of the lower pannels. Defaults to "X". Set to None to same as xlabel_up.
-        ylabel_up (str, list): y labels of the upper pannels. Defaults to r"T[$\\mu$K]".
+        ylabel_up (str, list): y labels of the upper pannels. Defaults to r"T [$\\mu$K]".
         ylabel_down (str, list): y labels of the lower pannels. Defaults to None to same as ylabel_up.
         color (str, list): colors of the lines. Defaults to None.
         marker (str, list): markers of the lines. Defaults to None.
@@ -518,9 +518,9 @@ def plot_profile_2c2r(
     fontsize: Union[float, List[float]] = [12.0] * 3,
     xlabel: Union[str, List[Union[str, None]], None] = [None, None, "Y", "X"],
     ylabel: Union[str, List[Union[str, None]], None] = [
-        r"$T\,[\mu$K] (HI only)",
+        r"$T\ [\mu$K] (HI only)",
         None,
-        r"$T\,[\mu$K] (HI + noise)",
+        r"$T\ [\mu$K] (HI + noise)",
         None,
     ],
     title: List[Union[str, None]] = [
@@ -710,7 +710,7 @@ def plot_hist_result(
     density: Union[bool, List[bool]] = True,
     histtype: Union[str, List[str]] = "step",
     title: Union[str, List[Union[str, None]], None] = ["HI only", "HI + noise"],
-    xlabel: Union[str, List[Union[str, None]], None] = r"$T\,[\mu$K]",
+    xlabel: Union[str, List[Union[str, None]], None] = r"$T\ [\mu$K]",
     ylabel: Union[str, List[Union[str, None]], None] = ["PDF", None],
     **kwargs,
 ) -> List[Axes]:
@@ -841,5 +841,373 @@ def compare_profiles(
         if label_outer:
             _axes[0].label_outer()
             _axes[1].label_outer()
+
+    return axes
+
+
+def plot_profile_fixwidth_2r(
+    x: List[List[Union[NDArray, Sequence[float]]]],
+    y: List[List[Union[NDArray, Sequence[float]]]],
+    cut: int = 1,
+    axes: Union[Tuple[Axes], List[Axes], None] = None,
+    y_err: Optional[List[List[Optional[Union[NDArray, Sequence[float]]]]]] = None,
+    x_err: Optional[List[List[Optional[Union[NDArray, Sequence[float]]]]]] = None,
+    text_pos: Optional[List[List[float]]] = None,
+    width: Union[float, List[float]] = 0.5,
+    fontsize: Union[float, List[float]] = 12,
+    title: Union[str, List[str], None] = None,
+    xlabel: Union[str, List[Union[str, None]], None] = ["Y", "X"],
+    ylabel: Union[str, List[Union[str, None]], None] = r"$T\ [\mu$K]",
+    color: Union[str, List[str], None] = None,
+    marker: Union[str, List[str], None] = None,
+    linestyle: Union[str, List[str]] = "--",
+    label: Union[str, List[str], None] = None,
+    tick_in: bool = True,
+) -> Union[List[Axes], Tuple[Axes]]:
+    """
+    plot lines in two row subplots.
+
+    Args:
+        x (List[list]): x data points.
+        y (List[list]): y data points.
+        cut (int): cut index to separate the data into upper and bottom two parts. Defaults to 1. \
+            i.e. The upper and bottom panels of the first subplot is showing x[0, :cut] and x[0, cut:] respectively.
+        axes (List[Axes], Tuple[Axes], None): axes object. Defaults to None.
+        y_err (list, optional): y error of the data. Defaults to None.
+        x_err (list, optional): x error of the data. Defaults to None.
+        text_pos (list, optional): text positions. Defaults to None, and the text position will be auto.
+        width (float, list): width of the filament to show on the upper pannel. Defaults to 0.5.
+        fontsize (float): fontsize of the text. Defaults to 12.
+        title (str, list): titles of the subplots. Defaults to None.
+        xlabel_up (str, list): x labels of the upper pannels. Defaults to "Y".
+        xlabel_down (str, list): x labels of the lower pannels. Defaults to "X". Set to None to same as xlabel_up.
+        ylabel_up (str, list): y labels of the upper pannels. Defaults to r"T[$\\mu$K]".
+        ylabel_down (str, list): y labels of the lower pannels. Defaults to None to same as ylabel_up.
+        color (str, list): colors of the lines. Defaults to None.
+        marker (str, list): markers of the lines. Defaults to None.
+        linestyle (str, list): linestyles of the lines. Defaults to '--'.
+        label (str, list): labels of the lines. Defaults to None.
+        tick_in (bool): whether to show the ticks inside the subplots. Defaults to True.
+    """
+
+    ncol = len(x)
+    nline = len(x[0])
+    nrow = 2
+    ntext = 2
+
+    if axes is None:
+        _, axes = make_figure(
+            2,
+            ncol,
+            figsize=(4 * ncol + 2, 8),
+            sharey="row",
+            sharex="row",
+            wspace=0,
+            hspace=0.4,
+            aspect=None,
+        )
+    if axes is None:
+        raise ValueError("axes is None.")
+
+    ## chenck line parameters
+    color = arg_list(color, nline, ncol)
+    marker = arg_list(marker, nline, ncol)
+    linestyle = arg_list(linestyle, nline, ncol)
+    label = arg_list(label, nline, ncol)
+
+    # check text parameters
+    if text_pos is not None:
+        text_pos = arg_list(text_pos, ntext, ncol)
+    fontsize = arg_list(fontsize, ntext, ncol)
+    width = arg_list(width, ncol)
+
+    # check xlabels
+    xlabel = arg_list(xlabel, nrow * ncol)
+    ylabel = arg_list(ylabel, nrow * ncol)
+    title = arg_list(title, nrow * ncol)
+
+    for i in range(ncol):
+        st, ed = i * nline, (i + 1) * nline
+        try:
+            x_up, x_down = x[i][:cut], x[i][cut:]
+            y_up, y_down = y[i][:cut], y[i][cut:]
+            c_up, c_down = color[st:ed][:cut], color[st:ed][cut:]
+            m_up, m_down = marker[st:ed][:cut], marker[st:ed][cut:]
+            ls_up, ls_down = linestyle[st:ed][:cut], linestyle[st:ed][cut:]
+            la_up, la_down = label[st:ed][:cut], label[st:ed][cut:]
+
+            y_err_up, y_err_down = None, None
+            if y_err and y_err[i]:
+                y_err_up, y_err_down = y_err[i][:cut], y_err[i][cut:]
+
+            x_err_up, x_err_down = None, None
+            if x_err and x_err[i]:
+                x_err_up, x_err_down = x_err[i][:cut], x_err[i][cut:]
+
+            _text_pos = (
+                text_pos[i * ntext : (i + 1) * ntext] if text_pos is not None else None
+            )
+            _xlabel = xlabel[i * nrow : (i + 1) * nrow]
+            _ylabel = ylabel[i * nrow : (i + 1) * nrow]
+            _title = title[i * nrow : (i + 1) * nrow]
+
+        except IndexError as ie:
+            raise ValueError("cut index is out of the range.") from ie
+
+        # plot upper pannel
+        ax_up = axes[0, i] if ncol > 1 else axes[0]  # pyright: ignore[reportArgumentType, reportCallIssue]
+        plot_line(
+            x_up,
+            y_up,
+            ax_up,
+            y_err=y_err_up,
+            x_err=x_err_up,
+            xlabel=_xlabel[0],
+            ylabel=_ylabel[0],
+            title=_title[0],
+            color=c_up,
+            marker=m_up,
+            linestyle=ls_up,
+            label=la_up,
+            tick_in=tick_in,
+        )
+        ax_up.axhline(0, linestyle="--", c="gray")
+
+        ax_down = axes[1, i] if ncol > 1 else axes[1]  # pyright: ignore[reportGeneralTypeIssues, reportArgumentType, reportCallIssue]
+        plot_line(
+            x_down,
+            y_down,
+            ax_down,
+            y_err=y_err_down,
+            x_err=x_err_down,
+            xlabel=_xlabel[1],
+            ylabel=_ylabel[1],
+            title=_title[1],
+            color=c_down,
+            marker=m_down,
+            linestyle=ls_down,
+            label=la_down,
+            tick_in=tick_in,
+        )
+        ax_down.axhline(0, linestyle="--", c="gray")
+
+        # filament
+        mf = np.mean(y_down[0])
+        if y_err_down and y_err_down[0] is not None:
+            mf_err = np.sqrt(np.sum(y_err_down[0] ** 2)) / len(y_down[0])  # pyright: ignore[reportOperatorIssue]
+        else:
+            mf_err = np.std(y_down[0]) / np.sqrt(len(y_down[0]))
+
+        strf = r"$<T_{\rm f}>$ = %.2f $\pm$ %.2f $\mu$K" % (mf, mf_err)
+        if _text_pos is None:
+            ax_down.text(
+                0.05,
+                0.9,
+                strf,
+                color="r",
+                fontsize=fontsize[1],
+                transform=ax_down.transAxes,
+            )
+        else:
+            ax_down.text(*_text_pos[0], s=strf, color="r", fontsize=fontsize[1])
+
+        # background
+        mb = np.mean(y_down[1])
+        if y_err_down and y_err_down[1] is not None:
+            mb_err = np.sqrt(np.sum(y_err_down[1] ** 2)) / len(y_down[1])  # pyright: ignore[reportOperatorIssue]
+        else:
+            mb_err = np.std(y_down[1]) / np.sqrt(len(y_down[1]))
+
+        strb = r"$<T_{\rm bg}>$ = %.2f $\pm$ %.2f $\mu$K" % (mb, mb_err)
+        if _text_pos is None:
+            ax_down.text(
+                0.05,
+                0.8,
+                strb,
+                color="k",
+                fontsize=fontsize[2],
+                transform=ax_down.transAxes,
+            )
+        else:
+            ax_down.text(*_text_pos[1], s=strb, color="k", fontsize=fontsize[2])
+
+    return axes
+
+
+def plot_profile_fixwidth_2c(
+    x: List[Union[NDArray, Sequence[float]]],
+    y: List[Union[NDArray, Sequence[float]]],
+    cut: int = 1,
+    axes: Optional[List[Axes]] = None,
+    y_err: Optional[List[Optional[Union[NDArray, Sequence[float]]]]] = None,
+    x_err: Optional[List[Optional[Union[NDArray, Sequence[float]]]]] = None,
+    text_pos: Optional[List[List[float]]] = None,
+    width: float = 0.5,
+    fontsize: Union[float, List[float]] = 12,
+    xlabel: Union[str, List[Union[str, None]], None] = ["Y", "X"],
+    ylabel: Union[str, List[Union[str, None]], None] = [r"$T\ [\mu$K]", None],
+    title: Union[str, List[Union[str, None]], None] = [
+        "Transverse-section profile",
+        "Lengthwise-section profile",
+    ],
+    color: Union[str, List[str], None] = ["b", "r", "k"],
+    marker: Union[str, List[str], None] = [".", "o", "o"],
+    linestyle: Union[str, List[str], None] = "--",
+    label: Union[str, List[Union[str, None]], None] = [
+        None,
+        r"$T_{\rm f}$",
+        r"$T_{\rm bg}$",
+    ],
+    tick_in: bool = True,
+) -> List[Axes]:
+    """
+    plot profiles in two column subplots.
+
+    Parameters:
+        x (list): x data points.
+        y (list): y data points.
+        cut (int): separate the data points into two parts, \
+            e.g. cut=2 means the data points in the first two rows are plotted in the first subplot, \
+                and rest are plotted in the second subplot.
+        axes (Axes, optional): axes object. Defaults to None.
+        y_err (list, optional): y error of the data. Defaults to None.
+        x_err (list, optional): x error of the data. Defaults to None.
+        text_pos (List[list], optional): text position. Defaults to None, and the text position will be auto.
+        width (float): width of the filament to show on the left panel. Defaults to 0.5.
+        fontsize (str): fontsize of the text. Defaults to 12.
+        xlabel (str, list): x-axis labels. Defaults to ["Y", "X"].
+        ylabel (str, list): y-axis labels. Defaults to [r"$T$ [$\\mu$K]", None].
+        title (str, list): subplot titles. Defaults to ["Transverse-section profile", \
+            "Lengthwise-section profile"].
+        color (str, list): line colors. Defaults to ["b", "r", "k"].
+        marker (str, list): line markers. Defaults to [".", "o", "o"].
+        linestyle (str, list): line styles. Defaults to "--".
+        label (str, list): line labels. Defaults to [r"$T_{\rm f}$", r"$T_{\rm bg$"].
+        tick_in (bool): whether to show ticks inside the axes. Defaults to True.
+    Returns:
+        axes (list): axes object.
+
+    """
+    if axes is None:
+        _, axes = make_figure(
+            1,
+            2,
+            figsize=(8, 3),
+            sharey=True,
+            sharex=False,
+            wspace=0,
+            aspect=None,
+        )
+    if axes is None:
+        raise ValueError("axes is None.")
+
+    try:
+        x_left, x_right = x[:cut], x[cut:]
+        y_left, y_right = y[:cut], y[cut:]
+    except IndexError as ie:
+        raise ValueError("cut index is out of the range.") from ie
+
+    y_err_left, y_err_right = None, None
+    if y_err:
+        y_err_left, y_err_right = y_err[:cut], y_err[cut:]
+
+    x_err_left, x_err_right = None, None
+    if x_err:
+        x_err_left, x_err_right = x_err[:cut], x_err[cut:]
+
+    nline = len(x)
+    xlabel = arg_list(xlabel, 2)
+    ylabel = arg_list(ylabel, 2)
+    title = arg_list(title, 2)
+
+    color = arg_list(color, nline)
+    marker = arg_list(marker, nline)
+    linestyle = arg_list(linestyle, nline)
+    label = arg_list(label, nline)
+    fontsize = arg_list(fontsize, 3)
+
+    for i, ax in enumerate(list(axes)):  # type: ignore
+        c = color[:cut] if i == 0 else color[cut:]
+        m = marker[:cut] if i == 0 else marker[cut:]
+        ls = linestyle[:cut] if i == 0 else linestyle[cut:]
+        lb = label[:cut] if i == 0 else label[cut:]
+        plot_line(
+            x_left if i == 0 else x_right,
+            y_left if i == 0 else y_right,
+            ax=ax,
+            y_err=y_err_left if i == 0 else y_err_right,
+            x_err=x_err_left if i == 0 else x_err_right,
+            xlabel=xlabel[i],
+            ylabel=ylabel[i],
+            title=title[i],
+            color=c,
+            marker=m,
+            linestyle=ls,
+            label=lb,
+            tick_in=tick_in,
+        )
+        # add horizontal lines that marker T = 0
+        ax.axhline(0, linestyle="--", c="gray")
+        # add text that marker the mean values of filament and background.
+        if i == 1:
+            # filament
+            mf = np.mean(y_right[0])
+            if y_err_right and y_err_right[0] is not None:
+                mf_err = np.sqrt(np.sum(y_err_right[0] ** 2)) / len(y_right[0])  # pyright: ignore[reportOperatorIssue]
+            else:
+                mf_err = np.std(y_right[0]) / np.sqrt(len(y_right[0]))
+
+            strf = r"$<T_{\rm f}>$ = %.2f $\pm$ %.2f $\mu$K" % (mf, mf_err)
+            if text_pos is None:
+                ax.text(
+                    0.05,
+                    0.9,
+                    strf,
+                    color="r",
+                    fontsize=fontsize[0],
+                    transform=ax.transAxes,
+                )
+            else:
+                ax.text(*text_pos[0], s=strf, color="r", fontsize=fontsize[0])
+
+            # background
+            mb = np.mean(y_right[1])
+            if y_err_right and y_err_right[1] is not None:
+                mb_err = np.sqrt(np.sum(y_err_right[1] ** 2)) / len(y_right[1])  # pyright: ignore[reportOperatorIssue]
+            else:
+                mb_err = np.std(y_right[1]) / np.sqrt(len(y_right[1]))
+
+            strb = r"$<T_{\rm bg}>$ = %.2f $\pm$ %.2f $\mu$K" % (mb, mb_err)
+            if text_pos is None:
+                ax.text(
+                    0.05,
+                    0.8,
+                    strb,
+                    color="k",
+                    fontsize=fontsize[1],
+                    transform=ax.transAxes,
+                )
+            else:
+                ax.text(*text_pos[1], s=strb, color="k", fontsize=fontsize[1])
+
+        # add text that marker the width of filament
+        else:
+            strw = r"$w_{\rm t} = %.2f$" % width
+            if text_pos is None:
+                ax.text(
+                    0.05,
+                    0.9,
+                    strw,
+                    color="r",
+                    fontsize=fontsize[2],
+                    transform=ax.transAxes,
+                )
+            else:
+                ax.text(
+                    *text_pos[2],
+                    s=strw,
+                    color="r",
+                    fontsize=fontsize[2],
+                )
 
     return axes
